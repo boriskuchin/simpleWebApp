de909a7f89075fff98aacc01ac32b1ae15dbc419
Boris Kuchin <bvkuchin@gmail.com>
2022-12-14 16:44:24 +0300

    homework 4

---
+++ src/main/java/ru/bvkuchin/hw4springjs/hw4springjs/repositories/ProductRepository.java
@@ -0,0 +1,40 @@
+package ru.bvkuchin.hw4springjs.hw4springjs.repositories;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+import ru.bvkuchin.hw4springjs.hw4springjs.models.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() {
+        products = new ArrayList<>(List.of(
+                new Product(1, "Шорты", 68.35d),
+                new Product(2, "Носки", 15.5d),
+                new Product(3, "Джинсы", 100d),
+                new Product(4, "Куртка", 150.65d),
+                new Product(5, "Шапка", 50.5d)
+        ));
+
+    }
+
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public Product getProductByID(Integer id) {
+        return products.stream().filter(product -> product.getId().equals(id)).findFirst().orElseThrow(() -> new RuntimeException());
+    }
+
+    public void deleteProduct(Integer id) {
+        System.out.println(id);
+        products.removeIf(p -> p.getId().equals(id));
+    }
+}
---
+++ src/test/java/ru/bvkuchin/hw4springjs/hw4springjs/Hw4springjsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bvkuchin.hw4springjs.hw4springjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4springjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
